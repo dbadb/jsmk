2f5c9f05bd57a5f9700ece3dce4de00dc542168a
dbadb <dana.batali@gmail.com>
2017-03-22 09:58:56 -0700

    Visual Studio 14 toolset, hobbling along.

---
+++ _Config.jsmk/tool/windows/vs/ar.js
@@ -0,0 +1,21 @@
+let ToolCli = jsmk.Require("tool_cli.js").Tool;
+
+class AR extends ToolCli
+{
+    constructor(ts, vsvers)
+    {
+        let exefile = "link";
+        let arg0 = jsmk.path.resolveExeFile(exefile, ts.BuildVars.VSToolsDir);
+        if(!arg0) throw new Error("Can't resolve link "+
+                                    ts.BuildVars.VSToolsDir);
+        super(ts, `vs${vsvers}/ar1`, {
+            Role:  "archiver/c",
+            ActionStage: "build",
+            Semantics: ToolCli.Semantics.ManyToOne,
+            DstExt: "a",
+            Invocation: [arg0, "-lib -nologo -out:${DSTFILE} ${SRCFILES}"]
+        });
+    }
+}
+
+exports.AR = AR;
