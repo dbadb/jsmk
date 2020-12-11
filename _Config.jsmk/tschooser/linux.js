5b4070431567aeee35ebf20a501df2adb7b1fd3a
Dana Batali <dana@cannerycoders.com>
2020-12-11 12:07:01 -0800

    updated toolset version to 83

--- _Config.jsmk/tschooser/linux.js
+++ _Config.jsmk/tschooser/linux.js
@@ -3,7 +3,7 @@ exports.GetToolsets = function()
     let gcc = jsmk.LoadConfig("toolset/gcc.js").Toolset;
     var result = [
         new gcc({
-            vers: "63"
+            vers: "83" // 8.3 on raspi4
          }),
     ];
     return result;
