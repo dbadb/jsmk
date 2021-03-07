ad814080f14a8d119ca5ef6b6f4a5fe876d05e82
Dana Batali <dana@cannerycoders.com>
2021-03-07 15:16:16 -0800

    default.js was overriding carefully chosen defaults in Policy.
    was confusing, since no user .jsmk/policy/default.js means we'd get this
    one whereas the user's one inherited from Policy.

--- _Config.jsmk/policy/default.js
+++ _Config.jsmk/policy/default.js
@@ -7,31 +7,9 @@ class DefaultPolicy extends Policy
     {
         let config =
         {
-            RootFile: "_Root.jsmk",
-            ProjFile: "_Proj.jsmk",
-            AllStages: ["nuke",
-                        "sync",
-                        "clean",
-                        "build",
-                        "unittest",
-                        "install",
-                        "package",
-                        "reposit",
-                        "test",
-                        "benchmark"],
-            BuildStages: ["build", "install"],
+            // add more Policy overrides here or let the users
+            // do so in ~/.jsmk/policy
             Deployment: "debug",
-            Flavor:  jsmk.GetHost().Platform,
-            BuildID: "head",
-            ProjectMatch: null,
-            ToolsetMatch: null,
-            SubsetMatch: null,
-            Package: "UnnamedPackage",
-            ToolsetNameTmplt: "${ToolsetName}-${TargetArch}-${TargetPlatform}",
-            BuildTargetTmplt: "${Toolset}-${Flavor}-${Deployment}",
-            BuiltDirTmplt: "${DomainDir}/.built/${Module}", // add BuildID
-            InstallDirTmplt: "${RootDir}/.install/${BuildTarget}/${Package}",
-            PackageDirTmplt: "${RootDir}/.package/${BuildTarget}/${Package}",
         };
         Object.assign(config, optConfig);
         super(config);
