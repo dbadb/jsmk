16e705f07dd86566e7d2314e29791fa47c1d76d2
Dana Batali <dana.batali@gmail.com>
2023-01-09 14:40:52 -0800

    More fixes for frameworks.

---
+++ _Config.jsmk/framework/Cocoa.js
@@ -0,0 +1,23 @@
+//
+// A Framework representing the target OS system-level
+// support.
+//
+let Framework = jsmk.Require("framework").Framework;
+let Tool = jsmk.Require("tool").Tool;
+let Arch = jsmk.Require("toolset").Arch;
+
+class Cocoa extends Framework
+{
+    constructor(name, version)
+    {
+    }
+
+    IsNative() { return true; }
+
+    ConfigureTaskSettings(task)
+    {
+        // no-op
+    }
+}
+
+exports.Framework = Cocoa;;
