4b0bab51c839bf059257fa8b7f3895d9821a7ea8
Dana Batali <dana.batali@gmail.com>
2023-05-27 14:02:16 -0700

    winston transport rotate file was causing problems.

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 /* global jsmk */
 global.jsmk = require("./lib/jsmk.js").NewJsmk();
 
+global.oldwarn = console.warn;
+console.warn = function()
+{
+    jsmk.WARNING("someone writing to console: " + JSON.stringify(arguments));
+}
+
+console.log = function()
+{
+    jsmk.WARNING("someone writing to console: " + JSON.stringify(arguments));
+}
+
 try
 {
     jsmk.BeginSession();
@@ -13,6 +24,7 @@ catch(e)
 }
 
 let err = null;
+jsmk.TRACE("DoBuilds");
 jsmk.DoBuilds()
     .then(()=>{
         jsmk.NOTICE("-- Done ----");
