0961bcfa39596b8cb9b540f5e77754c0f9b4ece5
Dana Batali <dana.batali@gmail.com>
2023-12-01 14:58:41 -0800

    update msvc version

--- _Config.jsmk/toolset/vs22.js
+++ _Config.jsmk/toolset/vs22.js
@@ -13,7 +13,7 @@
 var Foundation = require("./foundation.js").Foundation;
 
 const sdkVers = "10.0.22000.0";
-const vcVers = "14.37.32822"; // "14.36.32532"; // "14.35.32215";
+const vcVers = "14.38.33130"; // "14.37.32822"; // "14.36.32532"; // "14.35.32215";
 const vsDir = "C:/Program Files/Microsoft Visual Studio/2022/Community/";
 const sdkRoot = "C:/Program Files (x86)/Windows Kits";
 const msvcDir = `${vsDir}/VC/Tools/MSVC/${vcVers}`;
